f6af0fb5790fc961d5930d9233e26ee66691a8d1
Intisaar <xakiin6@gmail.com>
2018-08-13 22:26:56 +0100

    second commit

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build -t Server .
-docker run -p 5000:80 Server
+docker build -t aspnetcore-demo-app .
+docker run -p 5000:80 aspnetcore-demo-app
---
+++ src/AspNetCoreDemoApp/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreDemoApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore()
+                .AddCors()
+                .AddJsonFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseCors(builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                )
+                .UseMvcWithDefaultRoute();
+        }
+    }
+}
